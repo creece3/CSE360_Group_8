7c56ae53feced13082b2ac9eb1529840c508e20a
creece3 <creece3@asu.edu>
2019-11-30 16:22:31 -0700

    Inital Commit

---
+++ CSE_360_Group_8/src/Data.java
@@ -0,0 +1,41 @@
+/* Names and IDs: Christopher Reece creece3
+ * Assignment: Final Project
+ * 
+ * Description: 
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.io.*;
+
+/**
+ * Data class to...
+ */
+public class Data 
+{
+	public List<Float> data;
+	public List<String> history, list;
+	
+	public Data(String filePath)
+	{
+		data = new ArrayList<Float>();
+		history = new ArrayList<String>(); 
+		list = new ArrayList<String>();
+		
+		handleFile(filePath);
+	}
+	
+	void handleFile(String filePath) 
+	{
+		File file = new File(filePath);
+		
+		if(file.exists()) 
+		{
+			
+		}
+		else 
+		{
+			
+		}
+	}
+}
