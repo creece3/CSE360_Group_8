16d9f888ab590867728d7b5106a3f7d9c7a9b945
creece3 <creece3@asu.edu>
2019-11-30 16:37:55 -0700

    Added error strings

---
+++ CSE_360_Group_8/src/Errors.java
@@ -0,0 +1,7 @@
+public class Errors
+{
+	String fileNonexistent = "File does not exist: ",
+			fileInvalid = "File has an invalid extension: ",
+			dataInvalid = "File input data invalid: ",
+			entryInvalid = "User input data invalid: ";
+}
