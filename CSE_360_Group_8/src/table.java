8990ab029080001c6331c7ce1584ceff34a82db2
KaremG <51216091+KaremG@users.noreply.github.com>
2019-12-02 12:35:00 -0700

    Create table.java
    Used for addDelete.java

---
+++ CSE_360_Group_8/src/table.java
@@ -0,0 +1,20 @@
+public class table {
+	
+    private double value;
+
+    public table(){
+        this.value = 0;
+    }
+
+    public table (double value){
+        this.value = value;
+    }
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+}
