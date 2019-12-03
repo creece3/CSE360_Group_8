9a6209d4b631e1f700c5d2ac19f2cad84f2f2b0d
creece3 <47402397+creece3@users.noreply.github.com>
2019-12-03 13:59:01 -0700

    Update Tester.java

--- CSE_360_Group_8/src/Tester.java
+++ CSE_360_Group_8/src/Tester.java
@@ -3,10 +3,13 @@ public class Tester
 	public static void main(String[] args) 
 	{
 		Data d = new Data("a.txt");
+		d.insertData("asdas123123");
+		d.insertData("12.213");
+		d.insertData("0");
+		d.insertData("");
+		d.insertData("-123.213");
 		
-		d.sortData();
 		d.printData();
-		d.insertData("abc");
 		d.printErrors();
 	}
 }
