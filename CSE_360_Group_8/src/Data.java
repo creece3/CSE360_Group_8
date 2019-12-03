01656ee49e3468acb55d9e7fea0b8db1370234c4
creece3 <47402397+creece3@users.noreply.github.com>
2019-12-03 13:52:01 -0700

    Update Data.java

--- CSE_360_Group_8/src/Data.java
+++ CSE_360_Group_8/src/Data.java
@@ -6,6 +6,7 @@
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.io.*;
 
@@ -21,6 +22,14 @@ public class Data
 	private List<Float> data;
 	private List<String> history, errors;
 	
+	/**
+	 * Class constructor initializing default values for the min and max member
+	 * variables that bound the data that analysis is done on. It also creates
+	 * array lists to hold Float data imported from files or user input, to
+	 * hold the history of operations done, and to hold the history of errors.
+	 * 
+	 * @param filePath
+	 */
 	public Data(String filePath)
 	{
 		min = 0f;
@@ -33,21 +42,197 @@ public class Data
 		handleFile(filePath);
 	}
 	
+	/**
+	 * Function to set the min and max variables that bound the data that
+	 * analysis is done on. If the min and max values are not floats, an
+	 * error is added to the error arraylist.
+	 * 
+	 * @param min The min value to bound the data that analysis is done on.
+	 * @param max The max value to bound the data that analysis is done on.
+	 */
+	public void setMinMax(String min, String max) 
+	{
+    	try
+    	{
+    		this.min = Float.parseFloat(min);
+       	}
+    	catch(NumberFormatException e)
+    	{
+    		errors.add("Attempted to set invalid min: " + min);
+    	}
+    	
+    	try
+    	{
+    		this.max = Float.parseFloat(max);
+    	}
+    	catch(NumberFormatException e)
+    	{
+    		errors.add("Attempted to set invalid max: " + max);
+    	}
+	}
+	
+	/**
+	 * Function that prints the data array of floats in 4 neat columns. It first
+	 * sorts the data by calling the sort function. It then arranges the
+	 * elements of the data arraylist into 4 arrays that represent each column
+	 * to print. Lastly, it prints the elements of these arrays while calling
+	 * the trimFloat method which formats the floats to appear the same as they
+	 * were input.
+	 */
 	public void printData() 
 	{
 		sortData();
 		
 		int rows = data.size() / 4 + 1;
 		
-		for(Float dataElement : data) 
+		Float[] column1 = new Float[rows], 
+			column2 = null, 
+			column3 = null, 
+			column4 = null;
+
+		//From here down, arrange the elements of data into 4 Float arrays that
+		//represent the columns to print
+		for(int index = 0; index < rows; index++) 
 		{
-			if(dataElement >= min && dataElement <= max) 
+			column1[index] = data.get(index);
+		}
+		
+		if(data.size() % 4 == 1) 
+		{
+			column2 = new Float[rows - 1];
+			column3 = new Float[rows - 1];
+			column4 = new Float[rows - 1];
+			
+			for(int index = rows; index < rows * 2 - 1; index++) 
 			{
-				System.out.println(dataElement);
+				column2[index - rows] = data.get(index);
 			}
+			for(int index = rows * 2 - 1; index < 3 * rows - 2; index++) 
+			{
+				column3[index - (rows * 2 - 1)] = data.get(index);
+			}
+			for(int index = 3 * rows - 2; index < 4 * rows - 3; index++) 
+			{
+				column4[index - (3 * rows - 2)] = data.get(index);
+			}
+		}
+		else if(data.size() % 4 == 2) 
+		{
+			column2 = new Float[rows];
+			column3 = new Float[rows - 1];
+			column4 = new Float[rows - 1];
+			
+			for(int index = rows; index < rows * 2; index++) 
+			{
+				column2[index - rows] = data.get(index);
+			}
+			for(int index = rows * 2; index < 3 * rows - 1; index++) 
+			{
+				column3[index - rows * 2] = data.get(index);
+			}
+			for(int index = 3 * rows - 1; index < 4 * rows - 2; index++) 
+			{
+				column4[index - (3 * rows - 1)] = data.get(index);
+			}
+		}
+		else if(data.size() % 4 == 3) 
+		{
+			column2 = new Float[rows];
+			column3 = new Float[rows];
+			column4 = new Float[rows - 1];
+			
+			for(int index = rows; index < rows * 2; index++) 
+			{
+				column2[index - rows] = data.get(index);
+			}
+			for(int index = rows * 2; index < 3 * rows; index++) 
+			{
+				column3[index - rows * 2] = data.get(index);
+			}
+			for(int index = 3 * rows; index < 4 * rows - 1; index++) 
+			{
+				column4[index - rows * 3] = data.get(index);
+			}
+		}
+		else if(data.size() > 0 && data.size() % 4 == 0) 
+		{
+			column2 = new Float[rows];
+			column3 = new Float[rows];
+			column4 = new Float[rows];
+			
+			for(int index = rows; index < rows * 2; index++) 
+			{
+				column2[index] = data.get(index);
+			}
+			for(int index = rows * 2; index < 3 * rows; index++) 
+			{
+				column3[index] = data.get(index);
+			}
+			for(int index = 3 * rows; index < 4 * rows; index++) 
+			{
+				column4[index] = data.get(index);
+			}
+		}
+		
+		//Print the data
+		for(int index = 0; index < rows; index++) 
+		{
+			if(index < column1.length) 
+			{
+				System.out.printf("%-15s", trimFloat(column1[index]));
+			}
+			if(index < column2.length) 
+			{
+				System.out.printf("%-15s", trimFloat(column2[index]));
+			}
+			if(index < column3.length) 
+			{
+				System.out.printf("%-15s", trimFloat(column3[index]));
+			}
+			if(index < column4.length) 
+			{
+				System.out.printf("%-15s", trimFloat(column4[index]));
+			}
+			
+			System.out.println();
 		}
 	}
 	
+	/**
+	 * Function to format a Float value. It takes in the Float value to format,
+	 * and it converts it to a String. It then trims all trailing 0's from the
+	 * String. Lastly, it returns this String value.
+	 * 
+	 * @param floatToTrim The float to remove trailing 0's from.
+	 * @return String value that represents the float with no trailing 0's.
+	 */
+	public String trimFloat(Float floatToTrim) 
+	{
+		String trimmedFloat = floatToTrim.toString();
+		int index = trimmedFloat.length() - 1;
+		
+		while(index >= 1 && trimmedFloat.charAt(index) != '.') 
+		{
+			if(trimmedFloat.charAt(index) == '0') 
+			{
+				trimmedFloat = trimmedFloat.substring(0, index);
+			}
+			
+			index--;
+		}
+			
+		if(trimmedFloat.charAt(index) == '.' && trimmedFloat.length() == index + 1) 
+		{
+			trimmedFloat = trimmedFloat.substring(0, index);
+		}
+		
+		return trimmedFloat;
+	}
+	
+	/**
+	 * Function to print all the errors in the error arraylist. It simply
+	 * iterates through the errors and prints them each on a new line.
+	 */
 	public void printErrors() 
 	{
 		for(String error : errors) 
@@ -56,6 +241,16 @@ public class Data
 		}
 	}
 	
+	/**
+	 * Function to insert user input data into the data arraylist. It first
+	 * attempts to conver the user input string value into a Float value, and
+	 * if it can be converted it is added to the data arraylist and history is
+	 * kept of this action. If it cannot be converted to a float value, an error
+	 * is added to errors.
+	 * 
+	 * @param dataElementString The string that the user wants to insert into
+	 * the data arraylist.
+	 */
 	public void insertData(String dataElementString) 
 	{
 		Float dataElementFloat;
@@ -74,13 +269,46 @@ public class Data
     	}
 	}
 	
+	/**
+	 * Function to sort the elements of the data arraylist in descending order.
+	 * It does this by first initializing a Comparator<float> that returns true
+	 * if the first element is greater than the second element and so forth. It
+	 * then uses the Collections.sort method and the comparator to sort.
+	 */
 	public void sortData() 
 	{
-		Collections.sort(data);
-		
-		history.add(Events.sort);
+		Comparator<Float> comparator = new Comparator<Float>() 
+		{
+
+			@Override
+			public int compare(Float arg0, Float arg1)
+			{
+				if(arg0 > arg1) 
+				{
+					return -1;
+				}
+				else if(arg0 < arg1) 
+				{
+					return 1;
+				}
+				else 
+				{
+					return 0;
+				}
+			}
+		};
+				
+		Collections.sort(data, comparator);
 	}
-		
+	
+	/**
+	 * Function that checks if the filePath from the class constructor is valid.
+	 * If it is valid, it will call a function to read in the data. If it is
+	 * invalid, it will record and error in errors.
+	 * 
+	 * @param filePath The filePath from the class constructor to check if it is
+	 * valid and exists.
+	 */
 	private void handleFile(String filePath) 
 	{
 		File file = new File(filePath);
@@ -97,7 +325,6 @@ public class Data
 				} 
 				catch (IOException e) 
 				{
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
@@ -112,6 +339,16 @@ public class Data
 		}
 	}
 	
+	/**
+	 * Function that reads in the data from the file specified at the parameter
+	 * filePath. It reads each element into the data arraylist if the element is
+	 * valid. If it is not valid, an error is stored in the data arraylist.
+	 * 
+	 * @param filePath the filePath of the file to read from.
+	 * @throws IOException Exception thrown if the filePath is invalid. This
+	 * should not happen, because the filePath is checked in the function that
+	 * calls this function.
+	 */
 	private void readFile(String filePath) throws IOException 
 	{
 		int currentLine = 0;
