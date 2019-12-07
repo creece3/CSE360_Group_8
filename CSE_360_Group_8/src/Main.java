4e2b90ca3a59a33dd2a40396d4f4abd817acd2ec
John Fields <42076428+jackdfields@users.noreply.github.com>
2019-12-06 20:00:49 -0700

    Updated layout

--- CSE_360_Group_8/src/Main.java
+++ CSE_360_Group_8/src/Main.java
@@ -1,25 +1,14 @@
-
 import javafx.application.Application;
 import javafx.collections.ObservableList;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.text.Text;
-import javafx.scene.text.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.text.Text;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.geometry.Insets;
+import javafx.scene.text.*;
 
 public class Main extends Application {
 
@@ -30,13 +19,11 @@ public class Main extends Application {
     Button deleteButton;
     Data data = null;
     Text errors = new Text();
-    
-    //Historgram Data
+    Text dataSection = new Text();
 
-    List <Float> grades = new ArrayList<Float>();
-    int DATA_SIZE = 1000;
-    int hData[] = new int[DATA_SIZE];
-    int group[] = new int[10];
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -51,23 +38,37 @@ public class Main extends Application {
         dateEntryPane.setStyle("-fx-background-color: #336699;");
 
         // Boundaries for dataset
+        VBox inputDataPane = new VBox();
+        inputDataPane.setPadding(new Insets(15, 12, 15, 12));
+        inputDataPane.setSpacing(10);
+        inputDataPane.setStyle("-fx-background-color: #336699;");
         HBox boundaryPane = new HBox();
         boundaryPane.setPadding(new Insets(15, 12, 15, 12));
         boundaryPane.setSpacing(10);
         boundaryPane.setStyle("-fx-background-color: #336699;");
+        HBox reportButtonsPane = new HBox();
+        reportButtonsPane.setPadding(new Insets(15, 12, 15, 12));
+        reportButtonsPane.setSpacing(10);
+        reportButtonsPane.setStyle("-fx-background-color: #336699;");
+
+        inputDataPane.getChildren().addAll(boundaryPane, reportButtonsPane);
 
         //Value input
+        Text minLabel = new Text("Min: ");
         TextField minBoundaryInput = new TextField();
-        minBoundaryInput.setPromptText("Min");
+        minBoundaryInput.setText("0");
+        Text maxLabel = new Text("Max: ");
         TextField maxBoundaryInput = new TextField();
-        maxBoundaryInput.setPromptText("Max");
+        maxBoundaryInput.setText("100");
 
         //Buttons
         Button submitData = new Button("Submit Data");
         Button createReport = new Button("Create Report");
+        Button importFile = new Button("Import File");
 //        addButton.setOnAction(e -> addButtonClicked());
 
-        boundaryPane.getChildren().addAll(minBoundaryInput,maxBoundaryInput,submitData, createReport);
+        boundaryPane.getChildren().addAll(minLabel, minBoundaryInput, maxLabel, maxBoundaryInput, submitData);
+        reportButtonsPane.getChildren().addAll(createReport, importFile);
 
         //Value column
         TableColumn<table, Double> valueColumn = new TableColumn<>("Value");
@@ -105,7 +106,7 @@ public class Main extends Application {
         });
 
         // Add the panes to the dataEntry section
-        dateEntryPane.getChildren().addAll(boundaryPane, tablePane);
+        dateEntryPane.getChildren().addAll(inputDataPane, tablePane);
         root.getChildren().addAll(dateEntryPane);
 
         // Error Side
@@ -116,9 +117,6 @@ public class Main extends Application {
 
         Text errorHeader = new Text("ERROR:");
         errorHeader.setFont(font);
-
-        errors.setStyle("-fx-text-inner-color: red; -fx-font-size: 16px;");
-//        errors.setDisable(true);
 
         errorBox.getChildren().addAll(errorHeader, errors);
 
@@ -133,76 +131,12 @@ public class Main extends Application {
         Text dataHeader = new Text("DATA:");
         dataHeader.setFont(font);
 
-        Text dataDisplay = new Text();
-
-        dataBox.getChildren().addAll(dataHeader, dataDisplay);
+        dataBox.getChildren().addAll(dataHeader, dataSection);
 
         split_pane.getItems().addAll(dataBox);
 
         root.getChildren().addAll(split_pane);
-        
-        // Historgram
-        
-        /*
-            Random floats for Data data
-            FOR TESTING
-        */
-        //***********************
-        Data gradeList = new Data();
 
-        Random r = new Random();
-
-        for (int i = 0; i < DATA_SIZE; i++) {
-            Float random = 0 + r.nextFloat() * (100 - 0);
-            grades.add(random);
-            gradeList.insertData(random.toString());
-        }
-        groupData();
-        //***********************
-        Label labelInfo = new Label();
-        labelInfo.setText(
-                "Historgram: "  + "\n"
-                );
-
-        final CategoryAxis xAxis = new CategoryAxis();
-        final NumberAxis yAxis = new NumberAxis();
-        final BarChart<String, Number> barChart
-                = new BarChart<>(xAxis, yAxis);
-        barChart.setCategoryGap(0);
-        barChart.setBarGap(0);
-
-        xAxis.setLabel("Grades");
-        yAxis.setLabel("Frequency");
-
-        XYChart.Series series1 = new XYChart.Series();
-        series1.setName("Histogram");
-        series1.getData().add(new XYChart.Data("0-10", group[0]));
-        series1.getData().add(new XYChart.Data("10-20", group[1]));
-        series1.getData().add(new XYChart.Data("20-30", group[2]));
-        series1.getData().add(new XYChart.Data("30-40", group[3]));
-        series1.getData().add(new XYChart.Data("40-50", group[4]));
-
-        series1.getData().add(new XYChart.Data("50-60", group[5]));
-        series1.getData().add(new XYChart.Data("60-70", group[6]));
-        series1.getData().add(new XYChart.Data("70-80", group[7]));
-        series1.getData().add(new XYChart.Data("80-90", group[8]));
-        series1.getData().add(new XYChart.Data("90-100", group[9]));
-
-        barChart.getData().addAll(series1);
-
-        VBox HistvBox = new VBox();
-        HistvBox.getChildren().addAll(labelInfo, barChart);        
-
-        Scene histScene = new Scene(HistvBox, 800, 450);
-        Button histB = new Button("Make Historgram!");
-        
-        histB.setOnAction(e -> 
-        {
-            //groupData();
-            primaryStage.setScene(histScene);
-        });
-        root.getChildren().addAll(histB);
-        
         // display the Application
         primaryStage.setTitle("Grading Application");
         Scene scene = new Scene(root, 1000, 800); //setup of the scene inside the pane
@@ -210,15 +144,31 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-    //Add button clicked
+    /**
+     * Function to add inputted data to the Data Table
+     */
     private void addButtonClicked(){
+        // NEED TO CHECK DATA TYPE
+        if (data == null)
+            data = new Data();
+
         table valTable = new table();
         valTable.setValue(Double.parseDouble(valueInput.getText()));
         table.getItems().add(valTable);
+
+        // Insert and print data
+        data.insertData(valueInput.getText());
+        dataSection.setText(data.printData());
+
         valueInput.clear();
+
+        errors.setText(data.printErrors());
+
     }
 
-    //Delete button clicked
+    /**
+     * Function to remove selected data from the Data Table
+     */
     private void deleteButtonClicked(){
         ObservableList<table> productSelected, allProducts;
         allProducts = table.getItems();
@@ -226,50 +176,19 @@ public class Main extends Application {
         productSelected.forEach(allProducts::remove);
     }
 
-    // Boundaries for Data
+    /**
+     * Function to set the min and max variables that bound the data that
+     * analysis is done on. If the min and max values are not floats, an
+     * error is called and displayed to the Error TextArea
+     *
+     * @param minBound The min value to bound the data that analysis is done on.
+     * @param maxBound The max value to bound the data that analysis is done on.
+     */
     private void getBoundaries(TextField minBound, TextField maxBound){
         if (data == null)
             data = new Data();
         data.setMinMax(minBound.getText(), maxBound.getText());
 
         errors.setText(data.printErrors());
-    }
-    // Readies histogram
-    private void groupData()
-    {
-        //grades = data.getData();
-       
-        //grades.add(Data.data.get(i));
-        for(int i=0; i<10; i++){
-            group[i]=0;
-        }
-        for(int i=0; i<DATA_SIZE; i++)
-        {
-            if(grades.get(i)<=10){
-                group[0]++;
-            }else if(grades.get(i)<=20){
-                group[1]++;
-            }else if(grades.get(i)<=30){
-                group[2]++;
-            }else if(grades.get(i)<=40){
-                group[3]++;
-            }else if(grades.get(i)<=50){
-                group[4]++;
-            }else if(grades.get(i)<=60){
-                group[5]++;
-            }else if(grades.get(i)<=70){
-                group[6]++;
-            }else if(grades.get(i)<=80){
-                group[7]++;
-            }else if(grades.get(i)<=90){
-                group[8]++;
-            }else if(grades.get(i)<=100){
-                group[9]++;
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
